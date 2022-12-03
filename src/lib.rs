578fc7b93f2fe6ce63b1f3a89d22fce731432ea6
kieran.mckenzie <kieranjmckenzie@googlemail.com>
2022-12-03 07:08:38 +0000

    day3 p1

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,111 @@
-#[cfg(test)]
-pub mod tests {
-    use nom::{
-        character::complete::{digit1, newline},
-        combinator::{map, opt},
-        multi::many1,
-        sequence::terminated,
-        IResult,
+#![allow(dead_code, unused_imports, unused_macros)]
+
+
+
+use nom::{
+    character::complete::{digit1, newline},
+    combinator::{map, opt},
+    multi::many1,
+    sequence::terminated,
+    IResult,
+};
+
+macro_rules! problem {
+    ($x:expr, $y: expr) => {
+        include_str!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/",
+            $x,
+            "_",
+            $y,
+            ".txt"
+        ))
     };
+}
 
-    macro_rules! problem {
-        ($x:expr, $y: expr) => {
-            include_str!(concat!(
-                env!("CARGO_MANIFEST_DIR"),
-                "/",
-                $x,
-                "_",
-                $y,
-                ".txt"
-            ))
-        };
-    }
+#[test]
+fn day3_part1() {
+    let res: u32 = problem!(3, 1)
+        .lines()
+        .map(|line| {
+            let mut compartments = [0_u64; 2];
+            for (index, comp) in compartments.iter_mut().enumerate() {
+                let line_half_start = index * (line.len() / 2);
 
-    #[test]
-    fn day2_part1() {
-        let res = problem!(2, 1).lines().fold(0, |acc, line| {
-            let their = line.chars().nth(0).unwrap() as u32 - 65;
-            let mine = line.chars().nth(2).unwrap() as u32 - 23 - 65;
-            acc + 1
-                + if their == mine {
-                    mine + 3
-                } else if ((their | 1 << 2) - (mine | 0 << 2)) % 3 != 0 {
-                    mine
-                } else {
-                    mine + 6
-                }
-        });
-        println!("{}", res);
-    }
+                let line_half: &str = &line[line_half_start..line_half_start + line.len() / 2];
 
-    #[test]
-    fn day2_part2() {
-        let res = problem!(2, 1).lines().fold(0, |acc, line| {
-            let their = line.chars().nth(0).unwrap() as u32 - 65;
-            let outcome = line.chars().nth(2).unwrap();
-            acc + 1
-                + match outcome {
-                    'X' => {
-                        if their == 0 {
-                            2
-                        } else {
-                            their - 1
-                        }
+                line_half.chars().for_each(|c| {
+                    *comp |= 1_u64 << (c as u8 - b'A');
+                })
+            }
+
+            (match (compartments[0] & compartments[1]).trailing_zeros() as u8 + b'A' {
+                v @ b'a'..=b'z' => v - b'a' ,
+                v @ b'A'..=b'Z' => v - b'A'  + 26,
+                _ => panic!(),
+            }+1) as u32
+        })
+        .sum();
+    println!("{}", res);
+}
+
+#[test]
+fn day2_part1() {
+    let res = problem!(2, 1).lines().fold(0, |acc, line| {
+        let their = line.chars().nth(0).unwrap() as u32 - 65;
+        let mine = line.chars().nth(2).unwrap() as u32 - 23 - 65;
+        acc + 1
+            + if their == mine {
+                mine + 3
+            } else if ((their | 1 << 2) - (mine | 0 << 2)) % 3 != 0 {
+                mine
+            } else {
+                mine + 6
+            }
+    });
+    println!("{}", res);
+}
+
+#[test]
+fn day2_part2() {
+    let res = problem!(2, 1).lines().fold(0, |acc, line| {
+        let their = line.chars().nth(0).unwrap() as u32 - 65;
+        let outcome = line.chars().nth(2).unwrap();
+        acc + 1
+            + match outcome {
+                'X' => {
+                    if their == 0 {
+                        2
+                    } else {
+                        their - 1
                     }
-                    'Y' => their + 3,
-                    'Z' => (their | 1 << 2) % 3 + 6,
-                    _ => panic!(),
                 }
-        });
-        println!("{}", res);
-    }
+                'Y' => their + 3,
+                'Z' => (their | 1 << 2) % 3 + 6,
+                _ => panic!(),
+            }
+    });
+    println!("{}", res);
+}
 
-    #[test]
-    fn day1() {
-        let s = problem!(1, 1);
+#[test]
+fn day1() {
+    let s = problem!(1, 1);
 
-        let x: IResult<&str, Vec<u32>> = many1(terminated(
-            map(
-                many1(terminated(
-                    map(digit1, |s: &str| s.parse().unwrap()),
-                    opt(newline),
-                )),
-                |v: Vec<u32>| v.iter().sum(),
-            ),
-            opt(newline),
-        ))(s);
+    let x: IResult<&str, Vec<u32>> = many1(terminated(
+        map(
+            many1(terminated(
+                map(digit1, |s: &str| s.parse().unwrap()),
+                opt(newline),
+            )),
+            |v: Vec<u32>| v.iter().sum(),
+        ),
+        opt(newline),
+    ))(s);
 
-        let mut res = x.unwrap().1;
-        res.sort();
+    let mut res = x.unwrap().1;
+    res.sort();
 
-        println!("{}", res.iter().max().unwrap());
-        println!("{}", &res[res.len() - 3..].iter().sum::<u32>());
-    }
+    println!("{}", res.iter().max().unwrap());
+    println!("{}", &res[res.len() - 3..].iter().sum::<u32>());
 }
